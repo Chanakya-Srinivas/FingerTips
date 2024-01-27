d8e95024a0e6375add4004d61c2a52a4553e9a6c
Chanakya-Srinivas <chanu.korada98@gmail.com>
2024-01-27 12:00:40 -0600

    inital commit

--- test/searchresult_test.dart
+++ test/searchresult_test.dart
@@ -2,38 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mp5/models/newsnotifier.dart';
 import 'package:mp5/models/savedsearch.dart';
-import 'package:mp5/views/homescreen.dart';
+import 'package:mp5/views/searchresult.dart';
 import 'package:provider/provider.dart';
 
 late List<SavedSearch> searchList;
 
-Widget createHomeScreen() => ChangeNotifierProvider<NewsNotifier>(
+Widget createCardViewScreen() => ChangeNotifierProvider<NewsNotifier>(
       create: (context) => NewsNotifier(),
       child: MaterialApp(
-        home: HomeScreen(savedsearch: List.generate(20, (index) => SavedSearch(title: 'News App$index',icon: index<1 ? 'laptop' :'news')),),
+        home: SearchResult(notifier: NewsNotifier(), search: 'china',savedsearch: [
+          SavedSearch(title: 'china',icon: 'news',id: 123),
+        ],),
       ),
     );
 
 
 
 void main() {
-  group('Home Page Widget Tests', () {
-    testWidgets('Testing if ListView shows up', (tester) async {  
-      await tester.pumpWidget(createHomeScreen());
-      expect(find.byType(ListView), findsOneWidget);
-    });   
+  group('Edit Page Widget Tests', () {
 
-    testWidgets('Testing Scrolling', (tester) async {
-      await tester.pumpWidget(createHomeScreen());
-      expect(find.text('News App1',), findsOneWidget);
-      await tester.fling(
-        find.byType(ListView),
-        const Offset(0, -200),
-        3000,
-      );
-      await tester.pumpAndSettle();
-      expect(find.text('News App1'), findsNothing);
-    });
+      testWidgets('Testing IconButtons', (tester) async {
+        await tester.pumpWidget(createCardViewScreen());
+        expect(find.text('${'a'.toUpperCase()}uthor:'), findsOneWidget);
+        expect(find.text('${'s'.toUpperCase()}ource:'), findsOneWidget);
+        expect(find.text('${'c'.toUpperCase()}ontent:'), findsOneWidget);
+        expect(find.text('${'d'.toUpperCase()}escription:'), findsOneWidget);
+      });
   });
 
 }
